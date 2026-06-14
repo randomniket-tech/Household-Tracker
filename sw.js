0e087f2c57a99f655b3e075f2cd7e4d03e4a0bf0
randomniket-tech <randomniket@gmail.com>
2026-06-14 18:45:36 +0530

    Refactor service worker to use CACHE_NAME and ASSETS

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
+const CACHE_NAME = 'household-tracker-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+// Install Service Worker and cache core files
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('tracker-store').then((cache) => cache.addAll(['./index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fetch event handler (Strictly required for PWA installation)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
